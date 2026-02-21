c07024de38080bb9edf5bd9d7afb02a958b7ac16
Maksim Stashkevich <makstashkevich@gmail.com>
2026-02-21 10:52:06 +0300

    Add Main Terrex class for save common info

---
+++ terrex/main.py
@@ -0,0 +1,27 @@
+from terrex.player.player import Player
+
+
+class Main:
+    """
+    Main Terrex class for save common info
+    """
+
+    # players info
+    player: dict[int, Player] = {}
+    my_player_id: int = -1
+
+    # flags for special world seeds
+    # https://terraria.wiki.gg/wiki/Special_world_seeds
+    drunk_world: bool = False
+    get_good_world: bool = False
+    tenth_anniversary_world: bool = False
+    dont_starve_world: bool = False
+    not_the_bees_world: bool = False
+    remix_world: bool = False
+    no_traps_world: bool = False
+    zenith_world: bool = False
+    skyblock_world: bool = False
+    vampire_seed: bool = False
+    infected_seed: bool = False
+    team_based_spawns_seed: bool = False
+    dual_dungeons_seed: bool = False
