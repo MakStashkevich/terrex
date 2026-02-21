22bf877156f98d31d52eb79e02c625cc314d1166
Maksim Stashkevich <makstashkevich@gmail.com>
2026-02-21 10:57:08 +0300

    Update item logic

--- terrex/item/item.py
+++ terrex/item/item.py
@@ -1,3 +1,13 @@
-import collections
+from dataclasses import dataclass, field
 
-Item = collections.namedtuple('Item', 'item_id net_id position velocity prefix stacks')
+from terrex.net.vec2 import Vec2
+
+
+@dataclass
+class Item:
+    item_id: int = -1
+    net_id: int = -1
+    position: Vec2 = field(default_factory=lambda: Vec2())
+    velocity: Vec2 = field(default_factory=lambda: Vec2())
+    prefix: int = 0
+    stacks: int = 1
